c1030a3696e8737258e45dd40aad994c00e0cc0d
muditsharma1234 <mudit.bits@gmail.com>
2016-02-23 19:14:42 +0530

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
+## The function creates a matrix and computes its inversion if it is not present in the cache
+## If the inversion is present in the cache a cached copy of the inversion is returned
 ## functions do
 
-## Write a short comment describing this function
+## Creation of special vector which has list containing function to 
+## 1. Set Value of the matrix
+## 2. get the value of the vector
+## 3, Set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set <- function(x)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get<- function() x
+  setinverse <- function(inverse) m<<-inverse
+  getinverse <- function() m
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Diag calculates an identity matrix of size which is passed on to the create matrix function 
+## the function returns the inverted matrix incase a new matrix has been created else returns a
+## cached value of inversion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m))
+        {
+          message("Getting cached data")
+          return(m)
+        }
+      data<-x$get()
+      var1<-sqrt(length(data))
+      m<-solve(data,diag(var1))
+      x$setinverse(m)
+      m
+          
 }
